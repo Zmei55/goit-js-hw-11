189ca401f3395a24950c34b02b395546470c305d
Zmei55 <97633022+Zmei55@users.noreply.github.com>
2022-07-11 22:10:38 +0000

    Deploying to gh-pages from @ Zmei55/goit-js-hw-11@4727a3b52643d01a95262505683e67976859fafc 🚀

--- index.03076be1.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form"),document.querySelector("button.load-more"),document.querySelector(".gallery");
-//# sourceMappingURL=index.03076be1.js.map
--- index.6733b098.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form"),document.querySelector("button.load-more"),document.querySelector(".gallery");
-//# sourceMappingURL=index.6733b098.js.map
