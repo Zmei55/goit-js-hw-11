eb8b6dd1fdfe4babcddaf17f13635fc59ba66234
Andrei <andrei.lerke@gmail.com>
2022-07-11 21:12:38 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './css/styles.css';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  loadMoreBtn: document.querySelector('button.load-more'),
+  articlesContainer: document.querySelector('.gallery'),
+};
