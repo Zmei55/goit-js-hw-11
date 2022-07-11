d465b25b03548e9e3c12e91a191337dc04a232b2
Andrei <andrei.lerke@gmail.com>
2022-07-11 23:03:11 +0200

    fetch
    + template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import './css/styles.css';
+import pictureCardTpl from './templates/gallery-card.hbs';
+
+const KEY = '28160645-02600786ca706ffa5b60b520e';
+const BASE_URL = 'https://pixabay.com/api';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   loadMoreBtn: document.querySelector('button.load-more'),
   articlesContainer: document.querySelector('.gallery'),
 };
+
+fetch(
+  `${BASE_URL}/?key=${KEY}&q=yellow+flowers&page=1&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
+)
+  .then(r => r.json())
+  .then(picture => {
+    const markup = pictureCardTpl(picture.hits);
+    refs.articlesContainer.innerHTML = markup;
+  })
+  .catch(error => console.log(error));
