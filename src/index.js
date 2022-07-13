1640c18156182c7ee8a5d7074f94fb88a4918f24
Andrei <andrei.lerke@gmail.com>
2022-07-13 14:52:32 +0200

    Update index.js
    append class NewApiService

--- src/index.js
+++ src/index.js
@@ -1,55 +1,52 @@
 import './css/styles.css';
 import pictureCardTpl from './templates/gallery-card.hbs';
+import NewsApiService from './js/pictures-service';
 
-const KEY = '28160645-02600786ca706ffa5b60b520e';
-const BASE_URL = 'https://pixabay.com/api';
+// const KEY = '28160645-02600786ca706ffa5b60b520e';
+// const BASE_URL = 'https://pixabay.com/api';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   loadMoreBtn: document.querySelector('.load-more'),
-  articlesContainer: document.querySelector('.gallery'),
+  picturesContainer: document.querySelector('.gallery'),
 };
+
+const newsApiService = new NewsApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-function onLoadMore() {}
+function onLoadMore() {
+  newsApiService.fetchArticles().then(appendPicturesMarcup); // вызвали поиск из класса / нарисовали разметку
+}
 
 function onSearch(e) {
   e.preventDefault();
 
-  const form = e.currentTarget;
-  const searchQuery = form.elements.searchQuery.value;
+  newsApiService.query = e.currentTarget.elements.searchQuery.value; // записали input в класс, с пом set
 
-  fetchPictures(searchQuery)
-    .then(renderCard)
-    .catch(onFetchError)
-    .finally(() => form.reset());
-}
+  if (newsApiService.query === '') {
+    return console.log('Введи что-то нормальное');
+  }
 
-// fetch / запрос
-function fetchPictures(searchValue) {
-  const url = `${BASE_URL}/?key=${KEY}&q=${searchValue}&page=1&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`;
+  newsApiService.resetPage();
+  newsApiService.fetchArticles().then(pictures => {
+    clearPicturesContainer();
+    appendPicturesMarcup(pictures);
+  }); // вызвали поиск из класса / удалили старую разметку / нарисовали разметку
 
-  return fetch(url).then(r => {
-    if (r.ok) {
-      return r.json();
-    }
-
-    if (r === '') {
-      return console.log(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-
-    throw Error();
-  });
+  //   .catch(onFetchError)
+  //   .finally(() => form.reset());
 }
 
 // processing resolve / обработка хорошего результата
-function renderCard(picture) {
-  const markup = pictureCardTpl(picture.hits);
-  refs.articlesContainer.innerHTML = markup;
+function appendPicturesMarcup(pictures) {
+  refs.picturesContainer.insertAdjacentHTML('beforeend', pictureCardTpl(pictures)); // добавляем разметку не перерисовывая разметку
+}
+
+// очищаем страницу перед новым поиском
+function clearPicturesContainer() {
+  refs.picturesContainer.innerHTML = '';
 }
 
 // processing reject / обработка плохого результата
